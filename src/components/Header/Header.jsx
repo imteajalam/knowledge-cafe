cb9ee10315f643d7a23be090cbb04ac0ccc4c63d
imteajalam <imteajalam200@gmail>
2023-10-17 15:18:37 +0600

    Profile updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-import profile from '../../assets/images/profile.png'
+
 
 const Header = () => {
   return (
     <div className='flex justify-between items-center border-b-2 p-4 md:flex max-w-7xl mx-auto'>
       <h1 className='font-bold text-4xl'>React Knowledge Cafe</h1>
-      <img src={profile} alt="" />
+      <img className="w-16" src="https://i.ibb.co/nL1zSnz/boy2.jpg" alt="" />
       
     </div>
   );
